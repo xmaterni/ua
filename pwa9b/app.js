996b3b1311ab115c4db9179ea0ba249173025240
giuseppe materni <giuseppe.materni@gmail.com>
2022-11-18 23:25:28 +0100

    18-11-22 (23:25)

---
+++ pwa9b/app.js
@@ -0,0 +1,146 @@
+/*jshint esversion:8 */
+
+const rqs_type_test = "test";
+const rqs_type_log = "log";
+const rqs_type_cmd = "cmd";
+
+
+const log = function (...args) {
+  console.log(...args);
+};
+
+const ualog = function (...args) {
+  UaLog.log(...args);
+};
+
+const app_info = function (txt) {
+  msg_prn(hbar, txt);
+};
+
+const app_log = function (txt) {
+  msg_prn(m1, txt);
+};
+
+
+const SW_NAME = "/ua/pwaes4/sw.js";
+
+let SW_STATE = "unregistred";
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register(SW_NAME)
+    .then((registration) => {
+      let sw;
+      if (registration.installing) {
+        sw = registration.installing;
+        SW_STATE = "installing";
+        log('installing');
+        log(`scope: ${registration.scope}`);
+      } else if (registration.waiting) {
+        sw = registration.waiting;
+        SW_STATE = "waiting";
+        log('waiting');
+      } else if (registration.active) {
+        sw = registration.active;
+        navigator.serviceWorker.onmessage = receivesMessage;
+        SW_STATE = "active";
+        log('active');
+      }
+      if (sw) {
+        sw.addEventListener("statechange", (e) => {
+          log('statechange', e.target.state);
+        });
+      }
+      app_info(`SW ${SW_NAME}&nbsp;${SW_STATE.toUpperCase()}`);
+    })
+    .catch((error) => {
+      alert(`Error(0) app.js\n${SW_NAME}\n ${error}`);
+    });
+} else {
+  alert(`Erroro(1) app-js \n${SW_NAME} not in navigator`);
+}
+
+const json2str = function (js, ln = '\n') {
+  const es = Object.entries(js);
+  const lst = es.map((kv) => `${kv[0]}:${kv[1]}`);
+  return lst.join(ln);
+};
+
+//post message to worker
+const postMessageToWorker = function (msg) {
+  if (navigator.serviceWorker.controller) {
+    navigator.serviceWorker.controller.postMessage(msg);
+  } else {
+    alert(`Error(4) app.js ServiceWorker not activated \n ${SW_NAME}`);
+  }
+};
+
+const receivesMessage = function (event) {
+  const data = event.data;
+  const rsp_type = data.rsp_type || "";
+
+  if (rsp_type == rqs_type_test) {
+    const out = data.out || "";
+    if (out == 'prn') {
+      const html = json2str(data, "<br>");
+      app_log(html);
+    }
+    else if (out == "log") {
+      const txt = json2str(data, "<br>");
+      ualog(txt);
+      ualog("");
+    }
+    else {
+      alert(`Error(2) app.js response \n ${out}`);
+    }
+  }
+  else if (rsp_type == rqs_type_log) {
+    const txt = data.rsp;
+    ualog(txt);
+  }
+  else {
+    app_log(`Response Error <br> ${rsp_type}`);
+  }
+};
+
+
+function unregist() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister();
+    });
+  }
+}
+
+function unregistAll() {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (let registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+
+function clearCaches() {
+  caches.keys().then(function (names) {
+    for (let name of names)
+      caches.delete(name);
+  });
+}
+
+function reset() {
+  clearCaches();
+  unregistAll();
+}
+
+function reload() {
+  window.location.reload();
+}
+
+function toggleUaLog() {
+  const msg = {
+    rqs_type: rqs_type_cmd,
+    rqs: "toggle_ualog"
+  };
+  postMessageToWorker(msg);
+}
+
